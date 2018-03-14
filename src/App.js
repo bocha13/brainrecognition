e179059ca3f25a9586af0b152402937c10f18afb
ignacio bochatay <ignaciobochatay@gmail.com>
2018-03-14 11:45:34 -0300

    formating the files

--- src/App.js
+++ src/App.js
@@ -39,7 +39,6 @@ class App extends Component {
     
   }
 
-
   onButtonSubit = () => {
     this.setState({imageUrl: this.state.input})
     app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
